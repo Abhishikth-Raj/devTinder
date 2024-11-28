767cbe3bcedff29191dfdb4fd1585a7ab4b61786
Abhishikth2k1 <codewithraj1@gmail.com>
2024-11-28 19:36:35 +0530

    Adv rounting, regex and req query and params

--- src/app.js
+++ src/app.js
@@ -5,22 +5,46 @@ const app = express();
 
 //request handlers
 app.get("/user", (req,res)=>{
-    res.send({methodNroute: "app.get and user route", firstName : "Abhi", lastName : "N"});
+        console.log(req.query);
+    res.send(
+        {
+            firstName : "Abhi", 
+            lastName : "N"
+        });
 });
 
-app.post("/user", (req, res)=>{
-    res.send("User created Successfully");
-});
-app.patch("/user",(req,res)=>{
-    res.send("user modified successfully");
-});
-app.delete("/user",(req, res)=>{
-    res.send("user deleted successfully");
+app.get("/user1/:userId/:name/:password", (req,res)=>{
+        console.log(req.params);
+    res.send(
+        {
+            firstName : "Abhi", 
+            lastName : "N"
+        });
 });
 
-app.use("/test", (req, res)=>{
-    res.send("API Response: .use and test route");
+//Advanced routing techniques - again here the sequence matters
+app.get("/ab?c",(req,res)=>{
+    res.send("b in the url path is optional");
 });
+
+app.get("/ab+c",(req,res)=>{
+    res.send("b in the url can be repeated any number of times, path should start and end with a & c respectively");
+});
+
+app.get("/a(bc)?d",(req, res)=>{
+    res.send("bc together is optional");
+});
+
+//regex
+app.get(/a/, (req, res)=>{
+    res.send("any path that contains 'a' works");
+});
+
+app.get(/.*fly$/,(req, res)=>{
+    res.send("any path thta ends with fly works");
+});
+
+
 app.listen(7777, ()=>{
     console.log("Server is successfully listening on port 7777");
 });//server listens for requests
